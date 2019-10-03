75934a31e5a18372bb9a5580db647bb7b5e62b2e
Finley <finleyjchen@gmail.com>
2019-10-03 14:22:56 -0700

    change mobile header

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -31,11 +31,11 @@ class Menu extends React.Component {
     return (
       <header className="w-full bg-gray-800 border-pink-200 border-b-2">
         <nav className=" bg-gray-800 container mx-auto flex flex-col text-center content-center sm:flex-row sm:text-left sm:justify-between py-2 px-3 md:px-6 bg-white shadow sm:items-baseline w-full">
-          <div className="text-left mb-2 sm:mb-0 inner">
+          <div className="text-center md:text-left mb-2 sm:mb-0 inner">
 
           <Link
             to={'/'}
-            className="  text-2xl no-underline hover:text-blue-dark text-pink-200 hover:text-white font-bold"
+            className=" text-2xl no-underline hover:text-blue-dark text-blue-200 hover:text-white font-bold"
             >
             Finley Chen
             <span className="block text-sm font-medium">Frontend Web Developer<em></em></span>
@@ -49,7 +49,7 @@ class Menu extends React.Component {
           >
             <li className="inline-block mt-1 mr-3 md:mt-4 md:mr-4 lg:inline-block lg:mt-0">
               <Link
-                className="text-blue-200 hover:text-white"
+                className="text-pink-200 hover:text-white"
                 to={'/about'}
                 activeClassName="font-bold text-white"
               >
@@ -58,7 +58,7 @@ class Menu extends React.Component {
             </li>
             <li className="inline-block mt-1 mr-3 md:mt-4 md:mr-4 lg:inline-block lg:mt-0">
               <Link
-                className="text-blue-200 hover:text-white"
+                className="text-pink-200 hover:text-white"
                 to={'/work'}
                 activeClassName="font-bold text-white"
               >
@@ -67,7 +67,7 @@ class Menu extends React.Component {
             </li>
             <li className="inline-block mt-1 mr-3 md:mt-4 md:mr-4 lg:inline-block lg:mt-0">
               <Link
-                className="text-blue-200 hover:text-white"
+                className="text-pink-200 hover:text-white"
                 to={'/writing'}
                 activeClassName="font-bold text-white"
               >
@@ -78,7 +78,7 @@ class Menu extends React.Component {
               <Link
                 to={'/contact'}
                 activeClassName="font-bold text-white"
-                className="text-blue-200 hover:text-white"
+                className="text-pink-200 hover:text-white"
               >
                 Contact Me
               </Link>
