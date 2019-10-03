2825c7025c4f58251b8f49062fdb0324e73ed969
Finley <finleyjchen@gmail.com>
2019-10-03 14:14:17 -0700

    change footer social icon hover

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import SocialLinks from './SocialLinks'
 const Footer = () => (
   <footer className="w-full px-3 md:px-6 my-10 md:my-20 container mx-auto justify-between flex flex-col sm:flex-row items-center text-center text-xs">
     <p className="font-bold">&copy; Finley Chen</p>
-    <p className="">
+    <p className="footer-links">
       Crafted with {" "}
       <a href="https://www.gatsbyjs.org/">Gatsby</a>{" "} & {" "} <a href="https://tailwindcss.com">Tailwind</a> with <IoIosHeart className="inline-block" /> in San Luis Obispo
     </p>        
